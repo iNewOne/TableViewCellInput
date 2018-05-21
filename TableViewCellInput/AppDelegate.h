1e9e90ffe0b9d15480133e17e1dddb717001e3e9
许江飞 <xujf@gitblit>
2018-05-21 09:13:42 +0800

    Initial Commit

---
+++ TableViewCellInput/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewCellInput
+//
+//  Created by Xujiangfei on 2018/5/21.
+//  Copyright © 2018年 Xujiangfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableViewCellInput/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TableViewCellInput
+//
+//  Created by Xujiangfei on 2018/5/21.
+//  Copyright © 2018年 Xujiangfei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
